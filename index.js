f77471575dc8df87e704a52dc66311703e2a467e
shenczz <mrsaddam2806@gmail.com>
2023-08-24 20:40:11 +0700

    Task Day11

--- index.js
+++ index.js
@@ -1,18 +1,85 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const path = require("path");
+const { start } = require("repl");
+const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!!");
+// Setup call hbs with sub folder
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "src/public/views"));
+
+// Set serving static file
+app.use(express.static('src/assets'))
+
+// Set parsing
+app.use(express.urlencoded({ extended: false }))
+
+// Get Routing
+app.get('/', home)
+app.get('/add-project', formProject)
+app.get('/testimonial', testimonial)
+app.get('/contact', contact)
+app.get('/detail-project/:id', detailProject)
+
+// Post Routing
+app.post('/add-project', addProject)
+
+// Local Server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
-app.get("/users", (req, res) => {
-  res.send({
-    id: 1,
-    name: "SyencZ",
-  });
-});
+// Home
+function home(req, res) {
+  res.render('index')
+}
 
-app.listen(port, () => {
-  console.log(`Port running in Port ${port}`);
-});
+// Add Project Method Get / Post
+function formProject(req, res) {
+  res.render('add-project')
+}
+
+function addProject(req, res) {
+  const nameProject = req.body.inputProject
+  const startDate = req.body.inputStartDate
+  const endDate = req.body.inputEndDate
+  const description = req.body.inputDescription
+  const nodejs = req.body.nodejs
+  const golang = req.body.golang
+  const reactjs = req.body.reactjs
+  const javascript = req.body.javascript
+
+  console.log(nameProject)
+  console.log(startDate)
+  console.log(endDate)
+  console.log(description)
+  console.log(nodejs)
+  console.log(golang)
+  console.log(reactjs)
+  console.log(javascript)
+
+  res.redirect('/')
+}
+
+// Testimonial
+function testimonial(req, res) {
+  res.render('testimonial')
+}
+
+// Contact
+function contact(req,res) {
+  res.render('contact')
+}
+
+// Detail Project
+function detailProject(req,res) {
+  const { id } = req.params
+
+  const data = {
+    id,
+    title: "Dumbways Mobile App - 2022",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores a adipisci fugiat perspiciatis nisi magnam. Eum modi natus sunt quibusdam mollitia quod, maxime dolores. Porro rem officiis ad praesentium minima? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum pariatur nulla impedit, quasi, aliquam nesciunt consequuntur aliquid suscipit incidunt optio dolores, quis distinctio quae repellendus debitis aspernatur dolor error. Sit? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe, iste vero dolore delectus provident placeat explicabo, beatae tempora ratione alias eveniet sint laborum optio at omnis repellat mollitia odio aliquam."
+  }
+
+  res.render('detail-project', { data })
+}
---
+++ src/assets/Js/myProject.js
@@ -0,0 +1,112 @@
+let dataProject = [];
+
+function addedProject(event) {
+  event.preventDefault();
+
+  let projectName = document.getElementById("inputProject").value;
+  let startDate = document.getElementById("inputStartDate").value;
+  let endDate = document.getElementById("inputEndDate").value;
+  let description = document.getElementById("inputDescription").value;
+  let image = document.getElementById("inputImage").files;
+
+  const node = '<i class="fa-brands fa-node-js"></i>';
+  const golang = ' <i class="fa-brands fa-golang"></i>';
+  const react = '<i class="fa-brands fa-react"></i>';
+  const javascript = '<i class="fa-brands fa-square-js"></i>';
+
+  let nodeChecked = document.getElementById("nodejs").checked ? node : "";
+  let golangChecked = document.getElementById("golang").checked ? golang : "";
+  let reactChecked = document.getElementById("reactjs").checked ? react : "";
+  let javascriptChecked = document.getElementById("javascript").checked
+    ? javascript
+    : "";
+
+  image = URL.createObjectURL(image[0]);
+  // console.log(image);
+
+  let getDistanceTime = distanceTime();
+
+  let projectDataObject = {
+    projectName,
+    startDate,
+    endDate,
+    getDistanceTime,
+    nodeChecked,
+    golangChecked,
+    reactChecked,
+    javascriptChecked,
+    description,
+    image,
+  };
+
+  dataProject.push(projectDataObject);
+  // console.log(dataProject);
+
+  renderProject();
+}
+
+function renderProject() {
+  document.getElementById("container-card").innerHTML = "";
+  for (let i = 0; i < dataProject.length; i++) {
+    document.getElementById("container-card").innerHTML += `
+    <div class="card">
+    <div class="image-project">
+      <img src="${dataProject[i].image}" alt="image-project" />
+    </div>
+    <div class="title-and-duration">
+      <h3>
+        <a href="detail-project.html">${dataProject[i].projectName}</a>
+      </h3>
+      <p>Duration: ${dataProject[i].getDistanceTime}</p>
+    </div>
+    <div class="description-project">
+      <p>
+        ${dataProject[i].description}
+      </p>
+    </div>
+    <div class="tech-project">
+      ${dataProject[i].nodeChecked}
+      ${dataProject[i].golangChecked}
+      ${dataProject[i].reactChecked}
+      ${dataProject[i].javascriptChecked}
+    </div>
+    <div class="button-group">
+      <div class="btn-edit">
+        <button type="button">Edit</button>
+      </div>
+      <div class="btn-delete">
+        <button type="button">Delete</button>
+      </div>
+    </div>
+  </div>
+    `;
+  }
+}
+
+function distanceTime() {
+  let startDate = new Date(document.getElementById("inputStartDate").value);
+  let endDate = new Date(document.getElementById("inputEndDate").value);
+  let distance = new Date(endDate) - new Date(startDate);
+  let days = Math.floor(distance / (1000 * 3600 * 24));
+  let weeks = Math.floor(distance / (1000 * 3600 * 24 * 7));
+  let months = Math.floor(distance / (1000 * 3600 * 24 * 30));
+  let years = Math.floor(distance / (1000 * 3600 * 24 * 30 * 12));
+
+  if (years == 1) {
+    return `${years} year`;
+  } else if (years > 0) {
+    return `${years} years`;
+  } else if (months == 1) {
+    return `${months} month`;
+  } else if (months > 0) {
+    return `${months} months`;
+  } else if (weeks == 1) {
+    return `${weeks} week`;
+  } else if (weeks > 0) {
+    return `${weeks} weeks`;
+  } else if (days == 1) {
+    return `${days} day`;
+  } else if (days > 0) {
+    return `${days} days`;
+  }
+}
